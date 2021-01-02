0ecd3e5bf9bf2b368e9a18c95a6b51e2f666ff55
Jinwei <renjwjx@gmail.com>
2021-01-02 22:21:16 +0800

    dispatch barried

--- dispatch_栅栏/ViewController.m
+++ dispatch_栅栏/ViewController.m
@@ -20,4 +20,36 @@
 }
 
 
+- (IBAction)barrierBtn:(id)sender {
+ 
+    [self barrierRun];
+}
+
+
+- (void)barrierRun
+{
+    // thread3 should run after thread1 and thread2 complete
+    dispatch_queue_t queue = dispatch_queue_create("BarrierDemo", DISPATCH_QUEUE_CONCURRENT);
+     
+     dispatch_async(queue, ^{
+         for (int i = 0; i < 10; i++) {
+             NSLog(@"thread1 %@", [NSThread currentThread]);
+         }
+     });
+     dispatch_async(queue, ^{
+         for (int i = 0; i < 10; i++) {
+             NSLog(@"thread2 %@", [NSThread currentThread]);
+         }
+     });
+     dispatch_barrier_sync(queue, ^{
+         NSLog(@"========= dispatch_barrier ========");
+     });
+     
+     dispatch_async(queue, ^{
+         for (int i = 0; i < 10; i++) {
+             NSLog(@"thread3 %@", [NSThread currentThread]);
+         }
+     });
+}
+
 @end
